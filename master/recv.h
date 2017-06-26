35f7dbdf7f1be7d2810cb47c3d3ec05d244bdb00
gcy346447065 <gcy346447065@gmail.com>
2017-06-26 17:31:19 +0800

    delete master/ slave/

--- master/recv.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RECV_H_
-#define _RECV_H_
-
-int handle_sync_msg(const char *pcMsg, int iMsgLen);
-
-int recvFromSlaveSync(int iSyncSockFd, void *pMsg, int iMaxMsgLen);
-
-#endif //_RECV_H_
