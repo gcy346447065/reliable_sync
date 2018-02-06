977f91961a4500281d42e087b05bbcbbe8a8dde8
gcy346447065 <346447065@qq.com>
2018-02-06 12:04:04 +0800

    add include for sprintf open write

--- src/master_send.cpp
+++ src/master_send.cpp
@@ -1,5 +1,8 @@
 #include <stdlib.h> //for malloc
 #include <netinet/in.h> //for htons
+#include <stdio.h> // for sprintf
+#include <fcntl.h> // for open
+#include <unistd.h> // for write
 #include "master_send.h"
 #include "master.h"
 #include "macro.h"
